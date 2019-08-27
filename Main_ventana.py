97d096d8014a643f553981cd057a5dd8b319943c
JosAndr <jose.perezr@upb.edu.co>
2019-08-26 22:50:41 -0500

    UPD_GUI_facedect

--- Main_ventana.py
+++ Main_ventana.py
@@ -1,45 +1,107 @@
 import numpy
 import PIL
+import cv2
+import imutils
+import threading
+import os
+import time
+import tkinter as tk
 from tkinter import *
 from PIL import ImageTk,Image 
 from tkinter import ttk
-import cv2
+face_det=cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml')
+#Función para convertir matriz opencv a imagen tkinter a través de pillow
+#Entradas matriz imagen de opencv tamaño deseado
+#Salida imagen tkinter
+def CV2TK(matcv,x, y):
+    imagenpil=PIL.Image.fromarray(matcv)
+    imagenpil.thumbnail((x, y))
+    tempim=imagenpil.copy()
+    dispim= PIL.ImageTk.PhotoImage(tempim)
+    return dispim
+
+    
+  
+
+def show_frame():
+    _, frame = VS.read()
+    imagen= cv2.flip(frame, 1)
+    imnoroi=cv2.cvtColor(imagen, cv2.COLOR_BGR2RGBA)
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    faces = face_det.detectMultiScale(
+        gris,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(imagen,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gris[y:y+h, x:x+w]
+        roi_color = imagen[y:y+h, x:x+w]  
+    cv2image = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    visor.create_image(0,0, anchor=NW, image=imgtk)
+    visor.after(10,show_frame)
+    visor.update()
+    
+
 #Creando ventana principal
 mainwin=Tk()
-mainwin.geometry("711x528+100+100")
-mainwin.title('Raspynstagram')
-mainwin.resizable(0,0)
-#Creando barra de menú
-menubar=Menu(mainwin)
-mainwin.config(menu=menubar)
-#Creando menú de archivo
-archivo=Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Archivo", menu=archivo)
-archivo.add_command(label="Guardar")
-archivo.add_separator()
-archivo.add_command(label="Salir", command=quit)
-#Creando menú de ajustes
-ajustes=Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Ajustes", menu=ajustes)
-ajustes.add_command(label="Invertir Color")
-ajustes.add_command(label="Ajustar Contraste")
-filtrar=Menu(ajustes, tearoff=0)
-ajustes.add_cascade(label="Filtros", menu=filtrar)
-filtrar.add_command(label="Espaciales")
-filtrar.add_command(label="Frecuenciales")
-#Creando Lienzo principal
-visor=Canvas(mainwin,width=460,height=460)
-visor.place(x=21, y=29)
-imorig=cv2.cvtColor(cv2.imread("VLA.jpg"),cv2.COLOR_BGR2RGB)
-resim=cv2.resize(imorig,(460,460))
-dispim= PIL.ImageTk.PhotoImage(PIL.Image.fromarray(resim))
-visor.create_image(0,0, anchor=NW, image=dispim)
-#Creando Lienzo adicional
-visor2=Canvas(mainwin,width=200,height=200)
-visor2.place(x=501, y=29)
-imorig2=cv2.cvtColor(cv2.imread("VLA.jpg"),cv2.COLOR_BGR2GRAY)
-resim2=cv2.resize(imorig2,(200,200))
-dispim2= PIL.ImageTk.PhotoImage(PIL.Image.fromarray(resim2))
-visor2.create_image(0,0, anchor=NW, image=dispim2)
 
+mainwin.geometry("665x600+300+20")#Tamaño y ubicación de la ventana
+
+mainwin.title('Raspynstagram')#Nombre de la ventana
+
+mainwin.resizable(0,0)#Inhabilitando reescalado de la ventana
+
+#Creando barra de menú
+
+menubar=Menu(mainwin)
+
+mainwin.config(menu=menubar)
+
+#Creando menú de archivo
+
+archivo=Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Archivo", menu=archivo)
+
+archivo.add_command(label="Guardar")
+
+archivo.add_separator()
+
+archivo.add_command(label="Salir", command=quit)
+
+#Creando menú de ajustes
+
+ajustes=Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Ajustes", menu=ajustes)
+
+ajustes.add_command(label="Invertir Color")
+
+ajustes.add_command(label="Ajustar Contraste")
+
+filtrar=Menu(ajustes, tearoff=0)
+
+ajustes.add_cascade(label="Filtros", menu=filtrar)
+
+filtrar.add_command(label="Espaciales")
+
+filtrar.add_command(label="Frecuenciales")
+
+#Creando Lienzo principal
+
+visor=Canvas(mainwin,width=640,height=480) #defnición del tamaño del lienzo
+visor.place(x=21, y=29)#definición de la ubicación del lienzo
+
+visor.create_text(230,230, fill="darkblue",font="Verdana",
+                        text="Warming up.")
+time.sleep(2.0)
+VS=cv2.VideoCapture(0)
+show_frame()
 mainwin.mainloop()
+
+
+
--- facial test.py
+++ facial test.py
@@ -1,15 +1,26 @@
 import numpy as np
 import cv2
 face_det=cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml')
-img=cv2.imread('VLA.jpeg')
-gris=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_det.detectMultiScale(gris, 1.3, 5)
-for(x,y,w,h) in faces:
-   img=cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-   roi_gris=gris[y:y+h, x:x+w]
-   roi_col=img[y:y+h, x:x+w]
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows
-
-
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_det.detectMultiScale(
+        gray,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
